41dfebf99ad48c6bae0b5bc0217036cff81df58a
Kalela <philipkalela@gmail.com>
2019-01-18 15:39:24 +0300

    chore(OkHttp Logging): Set up logging interceptor
    -Set up logging interceptor
    
    [Delivers: 163187345]

--- app/src/main/java/com/example/antonynganga/android_freckle/utils/RetrofitInjector.kt
+++ app/src/main/java/com/example/antonynganga/android_freckle/utils/RetrofitInjector.kt
@@ -1,16 +1,28 @@
 package com.example.antonynganga.android_freckle.utils;
 
+import com.example.antonynganga.android_freckle.BuildConfig
 import com.example.antonynganga.android_freckle.data.Constants
 import com.example.antonynganga.android_freckle.services.AuthService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInjector {
+
+    val interceptor = if (BuildConfig.DEBUG) {
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    } else {
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+    }
+    val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
     fun provideRetrofit(baseUrl:String): Retrofit? {
         return Retrofit.Builder()
-            .baseUrl(baseUrl)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+                .baseUrl(baseUrl)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
     }
 
     fun provideService(): AuthService? {
