96600101bb2442c6e28051cd96fba3021a517e0a
meshnesh <tonnie.nesh@gmail.com>
2019-02-02 14:52:51 +0300

    W.I.P(User Login Functionality):
    - Develop User Login UI according to design specifications
    
    [finishes #163555717]

--- app/src/main/java/com/example/antonynganga/androidfreckle/MainActivity.kt
+++ app/src/main/java/com/example/antonynganga/androidfreckle/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.antonynganga.androidfreckle
 import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.example.antonynganga.androidfreckle.util.NetworkInfo
 import com.example.antonynganga.androidfreckle.data.Constants.CONNECTIVITY_ACTION
@@ -21,6 +22,9 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         registerReceiver(receiver, intentFilter)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
     }
 
     override fun onPause() {
