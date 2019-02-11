e587a67a2d9e8b69c5a8e06d0725f44c60f3889f
meshnesh <tonnie.nesh@gmail.com>
2019-02-11 12:30:50 +0300

    remove post clone file

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
 echo "Trying build script"
-# Example: Change bundle name of an iOS app for non-production
-if [ "$APPCENTER_BRANCH" != "ch-fix-failing-build" ];
-then
-    plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/MyApp/Info.plist
-fi
