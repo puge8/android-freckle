7e8d428b11843792644e86a016490364347bcc0d
meshnesh <tonnie.nesh@gmail.com>
2019-02-11 12:29:27 +0300

    creating post clone file

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-echo "This is the post clone"
+GOOGLE_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/<path>/google-services.json
+
+if [ -e "$GOOGLE_JSON_FILE" ]
+then
+    echo "Updating Google Json"
+    echo "$GOOGLE_JSON" > $GOOGLE_JSON_FILE
+    sed -i -e 's/\\"/'\"'/g' $GOOGLE_JSON_FILE
+
+    echo "File content:"
+    cat $GOOGLE_JSON_FILE
+fi
