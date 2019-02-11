bed77676354ad1c1c4456fe9e3f5746a6c518d1c
meshnesh <tonnie.nesh@gmail.com>
2019-02-11 11:46:59 +0300

    tetsting

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,9 +1,15 @@
+!#/bin/bash
+
+echo "================================================================================================================="
 GOOGLE_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/app/google-services.json
+
+echo "================================================================================================================="
 
 echo $APPCENTER_SOURCE_DIRECTORY
 
 if [ -e "$GOOGLE_JSON_FILE" ]
 then
+    echo "================================================================================================================="
     echo "Updating Google Json"
     echo "$GOOGLE_JSON" > $GOOGLE_JSON_FILE
     sed -i -e 's/\\"/'\"'/g' $GOOGLE_JSON_FILE
@@ -11,3 +17,4 @@ then
     echo "File content:"
     cat $GOOGLE_JSON_FILE
 fi
+echo "================================================================================================================="
